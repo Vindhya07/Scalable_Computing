a46420e79fde0e5773a58d60172abd90f92c52c0
Amith Nair <amithnair@Amiths-MacBook-Pro.local>
2021-10-04 12:24:04 +0100

    Code Files

---
+++ sample-code/ClassifyForLite.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import os
+import cv2
+import numpy
+import string
+import random
+import argparse
+import tensorflow as tf
+import tensorflow.keras as keras
+
+def getFinalOutput(numlist, interpreter):
+    return ''.join(decode("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", interpreter.get_tensor(x["index"])) for x in numlist)
+
+def decode(characters, y):
+    y = numpy.argmax(numpy.array(y), axis=1)
+    return ''.join([characters[x] for x in y])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model-name', help='Model name to use for classification', type=str)
+    parser.add_argument('--captcha-dir', help='Where to read the captchas to break', type=str)
+    parser.add_argument('--output', help='File where the classifications should be saved', type=str)
+    parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
+    args = parser.parse_args()
+
+    if args.model_name is None:
+        print("Please specify the CNN model to use")
+        exit(1)
+
+    if args.captcha_dir is None:
+        print("Please specify the directory with captchas to break")
+        exit(1)
+
+    if args.output is None:
+        print("Please specify the path to the output file")
+        exit(1)
+
+    if args.symbols is None:
+        print("Please specify the captcha symbols file")
+        exit(1)
+
+    symbols_file = open(args.symbols, 'r')
+    captcha_symbols = symbols_file.readline().strip()
+    symbols_file.close()
+
+    print("Classifying captchas with symbol set {" + captcha_symbols + "}")
+
+    with tf.device('/cpu:0'):
+        with open(args.output, 'w') as output_file:
+            interpreter = tf.lite.Interpreter(model_path="model.tflite")
+            interpreter.allocate_tensors()
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+
+
+            for x in os.listdir(args.captcha_dir):
+                # load image and preprocess it
+                raw_data = cv2.imread(os.path.join(args.captcha_dir, x))
+                rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
+                image = numpy.float32(numpy.array(rgb_data) / 255.0)
+                nterpreter = tf.lite.Interpreter(model_path="model.tflite")
+                interpreter.allocate_tensors()
+                input_details = interpreter.get_input_details()
+                output_details = interpreter.get_output_details()
+                interpreter.set_tensor(input_details[0]['index'], [image])
+
+                interpreter.invoke()
+                output_data = interpreter.get_tensor(output_details[0]['index'])
+                output_file.write(x + ", " + getFinalOutput(output_details, interpreter) + "\n")
+
+                print('Classified ' + x)
+
+if __name__ == '__main__':
+    main()
